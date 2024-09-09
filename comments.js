45cb0d2381c73d22c5eb06dac931afa438b4e277
Parthkumar Malaviya <malaviyaparth98@gmail.com>
2024-09-09 15:07:38 +0000

    Copilot Third Commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+//create web server
+var express = require('express');
+var app = express();
+
+//create a route
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.listen(3000, function(){
+    console.log('Server listening on port 3000');
+});
+//run server with node comments.js
